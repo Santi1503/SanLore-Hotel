1191e8af4f82b2e03517c7b4260d454ddf807023
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2024-12-07 11:39:40 +0100

    FIX

--- sanlore-hotel/back/Database/connection.js
+++ sanlore-hotel/back/Database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_CONNECTION)
+        await mongoose.connect("mongodb://localhost:27017/hotel")
         console.log("Conectado a la base de datos")
     } catch (error) {
         console.log(error)
